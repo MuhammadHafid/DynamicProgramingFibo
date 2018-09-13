75fa558ca40f7f5b3f6f9f059d4d9881eebb371b
ASUS <ASUS@LAPTOP-GS0UURLT>
2018-09-13 13:13:46 +0700

    Initial Commit

---
+++ src/HitungFibonacci/HitungFibonacci1.java
@@ -0,0 +1,18 @@
+package HitungFibonacci;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class HitungFibonacci1 {
+private static void tampilJudul(String identitas)  {
+    System.out.println("Identitas : "+ identitas);
+    
+    System.out.println("\nHitung Fibonacci");
+    System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+}
+    public static void main(String[] args) {
+        
+    String identitas = "Muhammad Hafid Azis / XRPL2 / 23";
+    tampilJudul(identitas);
+    }
+}
